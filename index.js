17365fb5c9ac50eef4936ab52c00d8892e3ddc60
obed004 <100470457+obed004@users.noreply.github.com>
2022-04-02 15:58:12 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+const startingMinutes = 05;
+let time = startingMinutes * 60;
+
+const countdownEl = document.getElementById('countdown');
+setInterval(updateCountdown, 1000);
+
+function updateCountdown() {
+    const Minutes = Math.floor(time / 60);
+    let seconds  = time % 60;
+    seconds = seconds < 10 ?  '0'+seconds:seconds
+    countdownEl.innerHTML = `${Minutes}: ${seconds}`;
+    time--; 
+} 
